8de36c66d8bf698d891ee320bacfd6c0febeacb3
rockaranza <diego.ocaranza@outlook.com>
2021-01-22 11:46:17 -0300

    codigo comentado y eliminacion de comprobaciones

--- src/script/script.js
+++ src/script/script.js
@@ -1,9 +1,9 @@
 
 function recogerDatos(){
+    //tomar el valor ingresado en el input para despues entregarlo a la API
     let nombrePoke = document.getElementById("nombrePokemon").value;
 
-    console.log(nombrePoke);
-
+    //consulta a la API con el nombre del pokemon ingresado anteriormente
     var request = $.ajax({
         url: `https://pokeapi.co/api/v2/pokemon/${nombrePoke}`,
         method: "GET"
@@ -20,16 +20,21 @@ function recogerDatos(){
 }
 
 function pokemonSalvaje(){
+    // pasar la etiqueta html a una variable para luego cambiar sus atributos
     var imprimirNombre = document.getElementById("nombreDePokemon");
     var imprimirImagen = document.getElementById("imagenPokemon");
+    //numero random entre 1 - 150 para mostrar pokemon aleatorio
     pokeNumero = Math.floor(Math.random() * (151 - 1)) + 1;
 
     var request = $.ajax({
+        //entregar el numero aleatorio para consultar en la API
         url: `https://pokeapi.co/api/v2/pokemon/${pokeNumero}`,
         method: "GET"
     });
     request.done(function( data ) {
+        //guardar la informacion en una variable para consultar lo que necesitemos  
         pokeAux=data;
+        //cambiamos los atributos con la informacion que aporta el pokeAux (infi guardada anteriormente)
         imprimirNombre.innerHTML = `${pokeAux.name.toUpperCase()}`;
         imprimirImagen.src = `${pokeAux.sprites.other.dream_world.front_default}`;
         console.log(data);
