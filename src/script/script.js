1b3ad5685236760baf845b21c4b32cc239fe7f64
rockaranza <diego.ocaranza@outlook.com>
2021-01-22 10:45:55 -0300

    Agregue boton random que muestra datos por pantalla de Imagen Pokemon y Nombre Pokemon

--- src/script/script.js
+++ src/script/script.js
@@ -9,6 +9,7 @@ function recogerDatos(){
         method: "GET"
     });
     request.done(function( data ) {
+        pokeAux=data;
         console.log(data);
     });
       
@@ -16,5 +17,26 @@ function recogerDatos(){
         console.log( 'Error: ' , error );
     });
 
+}
+
+function pokemonSalvaje(){
+    var imprimirNombre = document.getElementById("nombreDePokemon");
+    var imprimirImagen = document.getElementById("imagenPokemon");
+    pokeNumero = Math.floor(Math.random() * (151 - 1)) + 1;
+
+    var request = $.ajax({
+        url: `https://pokeapi.co/api/v2/pokemon/${pokeNumero}`,
+        method: "GET"
+    });
+    request.done(function( data ) {
+        pokeAux=data;
+        imprimirNombre.innerHTML = `${pokeAux.name.toUpperCase()}`;
+        imprimirImagen.src = `${pokeAux.sprites.other.dream_world.front_default}`;
+        console.log(data);
+
+    });
+    request.fail(function( error ) {
+        console.log( 'Error: ' , error );
+    });
 }
 
